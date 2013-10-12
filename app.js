0a762224e97e408e9ee40a4c3ddc3d355f567584
Ryo Suzuki <ryooopan@googlemail.com>
2013-10-12 10:12:25 +0900

    Add some translations

--- app.js
+++ app.js
@@ -29,30 +29,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-
-app.post('/translate', function(req, res) {
-  message = req.body.message
-  console.log(message);
-
-  var MeCab = new require('mecab-async');
-  var mecab = new MeCab();
-  mecab.wakachi(message, function(err, words) {
-    if (err) throw err;
-    console.log(words);
-
-    var result = ''
-
-    for (var i in words) {   
-      var word = words[i]
-      word = word.replace(/わたし|私|ぼく|僕/g, 'わて');
-      word = word.replace(/です/g, 'でおまんがな');
-      result = result + word;
-    }
-
-    res.json({ result: result }); 
-  });
-
-});
+app.post('/translate', routes.translate);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
