621b07a42e83fbdd4257be73a05dc35a4e012a17
Ryo Suzuki <ryooopan@googlemail.com>
2013-10-12 11:00:14 +0900

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-dengana
+Dengana
 =======
+
+## What's this
+
+東京の標準語を関西弁に自動翻訳するアプリケーションです。
+
+
+## Installation
+
+まず、Node.jsをインストールしていることが前提です。
+
+```
+git clone git@github.com:ryooopan/dengana.git
+cd dengana
+npm install
+```
+
+```
+node app.js
+```
--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,38 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
+exports.translate = function(req, res){
+  message = req.body.message
+  console.log(message);
+
+  message = message.replace(/ですか/g, 'でっか');
+  message = message.replace(/んです/g, 'ねん');
+  message = message.replace(/それじゃあ/g, 'ほな');
+
+  var MeCab = new require('mecab-async');
+  var mecab = new MeCab();
+  mecab.parse(message, function(err, words) {
+    if (err) throw err;
+
+    var result = ''
+    console.log(words);
+    for (var i in words) {
+      var word = words[i][0]
+      var kind = words[i][1]
+
+      word = word.replace(/わたし|私/g, 'うち');
+      word = word.replace(/ぼく|僕|オレ|俺|おれ/g, 'わて');
+      word = word.replace(/本当/g, 'ほんま');
+      word = word.replace(/違う/g, 'ちゃう');
+      word = word.replace(/です/g, 'でおまんがな');
+      word = word.replace(/じゃん/g, 'やん');
+      word = word.replace(/じゃあ/g, 'ほな');
+      result = result + word;
+    }
+
+    res.json({ result: result });
+  });
+
+};
+
+
